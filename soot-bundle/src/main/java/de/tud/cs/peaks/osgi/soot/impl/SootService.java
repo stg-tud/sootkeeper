e292ad692219c046a590c536b2a4c7cf4aad88a2
Patrick Müller <patrick.mueller@stud.tu-darmstadt.de>
2016-01-21 11:42:04 +0100

    Try to fix soot bundle

--- soot-bundle/src/main/java/de/tud/cs/peaks/osgi/soot/impl/SootService.java
+++ soot-bundle/src/main/java/de/tud/cs/peaks/osgi/soot/impl/SootService.java
@@ -7,9 +7,6 @@ import de.tud.cs.peaks.osgi.framework.api.data.IAnalysisResult;
 import de.tud.cs.peaks.osgi.soot.api.AbstractSootService;
 import de.tud.cs.peaks.osgi.soot.api.SootConfig;
 import de.tud.cs.peaks.osgi.soot.api.SootResult;
-import de.tud.cs.peaks.sootconfig.AnalysisTarget;
-import de.tud.cs.peaks.sootconfig.FluentOptions;
-import de.tud.cs.peaks.sootconfig.SootRun;
 import org.osgi.framework.BundleContext;
 
 import java.lang.instrument.IllegalClassFormatException;
@@ -45,13 +42,13 @@ public class SootService extends AbstractSootService {
         System.out.println("Prep. Soot!");
         try {
             soot.Main.main(new String[]{"-w", "-v", "-process-path", "/Users/floriankuebler/Desktop/securibench-91a/"});
-            FluentOptions options = new FluentOptions().wholeProgramAnalysis().keepLineNumbers().allowPhantomReferences();
-            AnalysisTarget target = new AnalysisTarget().processPath("/Users/floriankuebler/Desktop/securibench-91a/");
-            SootRun sootRun = new SootRun(options, target);
-            System.out.println("Start Soot");
-            de.tud.cs.peaks.sootconfig.SootResult res = sootRun.perform();
-            System.out.println("Soot has run:");
-            System.out.println(res);
+//            FluentOptions options = new FluentOptions().wholeProgramAnalysis().keepLineNumbers().allowPhantomReferences();
+//            AnalysisTarget target = new AnalysisTarget().processPath("/Users/floriankuebler/Desktop/securibench-91a/");
+//            SootRun sootRun = new SootRun(options, target);
+//            System.out.println("Start Soot");
+//            de.tud.cs.peaks.sootconfig.SootResult res = sootRun.perform();
+//            System.out.println("Soot has run:");
+//            System.out.println(res);
 
         } catch (Exception e) {
             e.printStackTrace();
